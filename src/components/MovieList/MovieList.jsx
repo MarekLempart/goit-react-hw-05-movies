b65a2dd36446073ed620a24d818631f06cfccd3e
MarekLempart <kama.zakupy@gmail.com>
2024-05-06 15:21:34 +0200

    Create MovieList.jsx

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,9 @@
+// MovieList.jsx
+
+import PropTypes from 'prop-types';
+
+const MovieList = { tredingMovies };
+
+MovieList.propTypes = {
+  tredingMovies: PropTypes.arrayOf(PropTypes.shape({})),
+};
